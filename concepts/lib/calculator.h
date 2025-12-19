359d51e3d847dab221524e5dc37e2147a5af1982
slemanz <william_sleman@hotmail.com>
2025-12-19 19:44:18 -0300

    concept(feat): add lib example

---
+++ concepts/lib/calculator.h
@@ -0,0 +1,15 @@
+#ifndef INC_CALCULATOR_H_
+#define INC_CALCULATOR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//unsigned int multiply(int a, int b);
+int multiply(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_CALCULATOR_H_ */
